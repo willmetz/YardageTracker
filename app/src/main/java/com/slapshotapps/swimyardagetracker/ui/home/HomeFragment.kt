acf69964c4217b66c505b74a9576d74803638284
Will Metz <willmetz@gmail.com>
2018-07-07 20:03:52 -0400

    some place holder work for actual data

--- app/src/main/java/com/slapshotapps/swimyardagetracker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/slapshotapps/swimyardagetracker/ui/home/HomeFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.slapshotapps.swimyardagetracker.R
+import com.slapshotapps.swimyardagetracker.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -15,16 +17,22 @@ class HomeFragment : Fragment() {
     }
 
     private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: HomeFragmentBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.home_fragment, container, false)
-    }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.home_fragment, container, false)
+
+        binding.item = viewModel
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/slapshotapps/swimyardagetracker/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/slapshotapps/swimyardagetracker/ui/home/HomeViewModel.kt
@@ -3,5 +3,20 @@ package com.slapshotapps.swimyardagetracker.ui.home
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    fun lastWorkoutYardage() : String{
+        return "1200 yards"
+    }
+
+    fun lastWorkoutDate(): String{
+        return "Friday July 6th 2018"
+    }
+
+    fun totalWorkoutsForYear() : String{
+        return "8"
+    }
+
+    fun totalYardageForYear(): String{
+        return "5000"
+    }
 }
