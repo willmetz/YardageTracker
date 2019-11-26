32d540cd0a32a2308363d092b33eb0ce1f5fed32
Will Metz <willmetz@gmail.com>
2019-11-26 09:54:17 -0500

    initial work to add in bottom navigation

--- app/src/main/java/com/slapshotapps/swimyardagetracker/MainActivity.kt
+++ app/src/main/java/com/slapshotapps/swimyardagetracker/MainActivity.kt
@@ -2,8 +2,10 @@ package com.slapshotapps.swimyardagetracker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import dagger.android.AndroidInjection
-
+import kotlinx.android.synthetic.main.main_activity.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +16,12 @@ class MainActivity : AppCompatActivity() {
         AndroidInjection.inject(this); // Call before super!
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setupNavigation()
+    }
+
+
+    private fun setupNavigation() {
+        val navController = findNavController(this, R.id.main_nav_fragment)
+        setupWithNavController(bottom_nav, navController)
     }
 }
