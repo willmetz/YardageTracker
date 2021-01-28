04b49d253ffe8944efea75510a522cec09659655
Will Metz <willmetz@gmail.com>
2021-01-27 21:41:10 -0500

    not sure if this is useful or not, added while testing out keeping transactions (didn't work)

--- app/src/androidTest/java/com/slapshotapps/swimyardagetracker/database/rules/IsMainExecutorRule.kt
+++ app/src/androidTest/java/com/slapshotapps/swimyardagetracker/database/rules/IsMainExecutorRule.kt
@@ -1,3 +1,35 @@
 package com.slapshotapps.swimyardagetracker.database.rules
 
+import androidx.arch.core.executor.ArchTaskExecutor
+import androidx.arch.core.executor.DefaultTaskExecutor
+import androidx.arch.core.executor.TaskExecutor
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
 
+
+class IsMainExecutorRule : TestWatcher() {
+
+    val defaultExecutor = DefaultTaskExecutor()
+
+    override fun starting(description: Description?) {
+        super.starting(description)
+        ArchTaskExecutor.getInstance().setDelegate(object : TaskExecutor() {
+            override fun executeOnDiskIO(runnable: Runnable) {
+                defaultExecutor.executeOnDiskIO(runnable)
+            }
+
+            override fun postToMainThread(runnable: Runnable) {
+                defaultExecutor.executeOnDiskIO(runnable)
+            }
+
+            override fun isMainThread(): Boolean {
+                return true
+            }
+        })
+    }
+
+    override fun finished(description: Description?) {
+        super.finished(description)
+        ArchTaskExecutor.getInstance().setDelegate(null)
+    }
+}
