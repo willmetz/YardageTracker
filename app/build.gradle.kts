285352d0d6bdd351037a5f14788bdf9ce6baccff
Will Metz <willmetz@gmail.com>
2026-07-22 22:17:00 -0400

    Target a newer version of the Android SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.slapshotapps.swimyardagetracker"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.slapshotapps.swimyardagetracker"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 9
         versionName = "1.6"
 
